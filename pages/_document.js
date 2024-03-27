792a4e9e1829a7facc55e723cdd8916f15ba8043
Kevin K. <kevinkim75@gmail.com>
2024-03-27 07:56:25 -0400

    Fixed GA4 script

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,13 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <script async src='https://www.googletagmanager.com/gtag/js?id=G-KP53EQEWPD' />
-          <script>
+          <script dangerouslySetInnerHTML={{ _html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-KP53EQEWPD');
-          </script>
+	    `
+	  }} />
 	  <link rel='icon' href='/favicon.ico' />
           <link rel='stylesheet' href='https://use.typekit.net/vrj7xjc.css' />
           <script src='https://kit.fontawesome.com/62785f7405.js' crossOrigin='anonymous' async />
